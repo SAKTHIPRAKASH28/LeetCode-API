961f9298b9af588359ab61d6f46d855fb4e64f32
Sakthi Prakash K <92044150+SAKTHIPRAKASH28@users.noreply.github.com>
2024-02-28 22:09:17 +0530

    added language level solved

--- main.py
+++ main.py
@@ -16,10 +16,19 @@ async def getData(username: str):
 
     if page.status_code == 200:
         soup = BeautifulSoup(page.content, 'html.parser')
-        # name =
+        languages = soup.find_all(
+            'div', class_='flex items-center justify-between text-xs text-label-1 dark:text-dark-label-1')
+        language_wise_solved = {}
+        for language in languages:
+            language_wise_solved[language.find(
+                'span', class_='inline-flex items-center px-2 whitespace-nowrap text-xs leading-6 rounded-full text-label-3 dark:text-dark-label-3 bg-fill-3 dark:bg-dark-fill-3 notranslate').text] = language.find('span', 'text-xs font-medium text-label-1 dark:text-dark-label-1').text
+
         data = {'username': soup.find(
             'div', class_='text-label-1 dark:text-dark-label-1 break-all text-base font-semibold').text,
             'rank': soup.find('span', class_='ttext-label-1 dark:text-dark-label-1 font-medium').text,
+            'language_wise_problems_solved': language_wise_solved,
+            'solved_count': {'easy'}
+
 
 
         }
