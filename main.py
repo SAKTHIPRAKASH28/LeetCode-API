fae92c0dd648905a031a27c5b79782147c33ad60
Sakthi Prakash K <92044150+SAKTHIPRAKASH28@users.noreply.github.com>
2024-02-28 21:51:50 +0530

    Adding Functionalities

--- main.py
+++ main.py
@@ -6,18 +6,24 @@ app = FastAPI()
 
 @app.get("/")
 async def home():
-    return ("Welcome to Instagram API. Read the github docs for more details")
+    return ("Welcome to LeetCode API. Read the github docs for more details")
 
 
-@app.get('/getUserdata/{username}')
+@app.get('/{username}')
 async def getData(username: str):
     url = f'https://leetcode.com/{username}/'
     page = requests.get(url)
 
     if page.status_code == 200:
         soup = BeautifulSoup(page.content, 'html.parser')
+        # name =
+        data = {'username': soup.find(
+            'div', class_='text-label-1 dark:text-dark-label-1 break-all text-base font-semibold').text,
+            'rank': soup.find('span', class_='ttext-label-1 dark:text-dark-label-1 font-medium').text,
 
-        
+
+        }
+        return data
 
     else:
         return ("User not found")
